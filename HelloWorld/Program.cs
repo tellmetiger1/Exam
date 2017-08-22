6594aae7709f4e4164844aabad373d41fa25d1b2
tellmetiger1 <tellmetiger@gmail.com>
2017-08-22 10:01:45 -0500

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloWorldLib;
+
+namespace HelloWorldMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldFactory factory = new HelloWorldFactory();
+            // create hellowWorld obj
+            IHelloWorld helloWorldObj = factory.CreateHelloWorldObj();
+
+            // call any of the implemented function to output "Hello World" to either console or database or other places
+            helloWorldObj.PrintHelloWorldToConsole("Hello World");
+        }
+    }
+}
